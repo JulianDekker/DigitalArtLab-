92a157ce2e3195012df9007ae505ada35a44679e
Moopsish <Julian.dekker@live.nl>
2015-12-01 17:12:01 +0100

    added print functionality

--- src/DigitalArtLabBundle/Resources/public/js/custom.js
+++ src/DigitalArtLabBundle/Resources/public/js/custom.js
@@ -18,5 +18,15 @@ $(document).ready(function(){
     var containerheight = viewheight - 141 - 150;
     $('.container.body').css({"min-height" : containerheight+"px" });
 
-
 });
+
+function appendFrame(username, index){
+    var frame = 'iframe[name=frame'+index+']';
+
+    if($(frame).length > 0){
+
+    }else{
+        $('body').append("<iframe style=\"display: none\" src=\"/digitalartlab/web/profile/" + username + "/print\" name=\"frame" + index + "\"></iframe>");
+    }
+
+}
